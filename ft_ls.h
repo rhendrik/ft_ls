7111eb629067c6edf3499c0edd3323d8f4649b4c
rhendrik <rhendrik@student.wethinkcode.co.za>
2019-07-10 21:12:49 +0200

    done planning for the night

---
+++ ft_ls.h
@@ -0,0 +1,17 @@
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#include "libft/libft.h"
+#include <dirent.h>
+#include <stat.h>
+#include <sys.h>
+#include <types.h>
+#include <grp.h>
+#include <xattr.h>
+#include <time.h>
+#include <unistd>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#endif
