50e1c2dbc77c21a34c67186d2c2b96e847beaf5b
rhendrik <rhendrik@student.wethinkcode.co.za>
2020-05-17 01:01:27 +0200

    still working on -l

--- src/print.c
+++ src/print.c
@@ -21,6 +21,26 @@ int print_tmp_l(char **tmp)
 	return (0);
 }
 
+char *permissions(struct stat statbuff)
+{
+	char *ret;
+
+	ret = ft_strnew(10);
+	if(statbuff.S_IRUSR)
+		ret[0] = 'r';
+	else
+		ret[0] ='-';
+	if(statbuff.S_IWUSR)
+		ret[1] = 'w';
+	else 
+		ret[1] = '-';
+	if(statbuff.S_IXUSR)
+		ret[2] = 'x';
+	else
+		ret[2] = '-';
+	return (ret);
+}
+
 void print_l(char *name)
 {
 	char *tmp;
@@ -37,8 +57,8 @@ void print_l(char *name)
 		if(S_ISREG(statbuff.st_mode) != 0)
 			tmp = ft_strcpy(tmp, "-");
 
-		/* permissiona */
-
+		/* permissions */
+		tmp = ft_strcat(tmp, permissions(statbuff));	
 		/* links */
 
 		/* owner name */
